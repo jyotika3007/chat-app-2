87624ff3f4cdb4be4650112a8b74eb71d88ccfdc
jyotika <jyotikasethi3007@gmail.com>
2022-02-19 01:07:03 +0530

    fixed

--- src/Login.js
+++ src/Login.js
@@ -2,22 +2,32 @@ import { Button } from '@material-ui/core';
 import { LocalGasStation } from '@material-ui/icons'
 import React from 'react'
 import './Login.css';
+import { auth, provider } from './firebase';
+import { actionTypes } from './reducer';
+import { useStateValue } from './StateProvider';
 
 function Login (){
 
-    const signIn = ()=>{
+    const [{user}, dispatch] = useStateValue();
 
+    const signIn = ()=>{
+        auth.signInWithPopup(provider).then(result => {
+            dispatch({
+                type: actionTypes.SET_USER,
+                user: result.user
+            })
+        }).catch(err => alert(err.message))
     }
 
     return (
         <div className="login">
             <div className="login__container">
-                <img src="" alt="" />
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/2042px-WhatsApp.svg.png" alt="" />
                 <div className="login__text">
                     <h1>Sign in to Chat App</h1>
                 </div>
 
-                <Button onClick="signIn">
+                <Button onClick={signIn}>
                     Sign In With Google
                 </Button>
             </div>
